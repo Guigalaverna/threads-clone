e4b8576598e0d166a4e3941d0f42b9aade73ffcc
Guilherme Galaverna <guigalaverna@gmail.com>
2023-07-10 02:16:39 -0300

    feat: add bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { Header } from "./components/Header";
 
 import { Post } from "./components/Post";
 
+import { GoHomeFill } from 'react-icons/go'
+import { FiEdit } from 'react-icons/fi'
+import { CgSearch } from 'react-icons/cg'
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsFillPersonFill } from 'react-icons/bs'
+
 export default function() {
   const [showingSplashScreen, setShowingSplashScreen] = useState(false);
 
@@ -12,13 +18,22 @@ export default function() {
   }
 
   return (
-    <main className="w-full h-full flex flex-col px-7 text-base">
+    <main className="w-full h-full flex flex-col px-3 text-base relative">
       <Header />
-      <section className="mt-5">
+      <section className="mt-5 mb-[7.7vh]">
         <Post />
         <Post />
         <Post />
       </section>
+      <footer className="fixed flex items-center justify-center w-full h-[8vh] bg-[#101010] bottom-0 left-0">
+        <div className="brightness-50 flex items-center justify-evenly w-full">
+          <GoHomeFill size={24} />
+          <CgSearch size={24} />
+          <FiEdit size={24} />
+          <AiOutlineHeart size={24} />
+          <BsFillPersonFill size={24} />
+        </div>
+      </footer>
     </main>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import ThreadsLogo from "../../assets/threads-logo.svg";
 export function Header() {
   return (
     <header className="h-[5vh] w-full flex items-center justify-center">
-      <img src={ThreadsLogo} alt="Logo do Threads" className="h-[30px] mt-5" />
+      <img src={ThreadsLogo} alt="Logo do Threads" className="h-[30px] mt-10" />
     </header>
   );
 }
